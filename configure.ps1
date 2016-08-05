7a9b8014aa4eaf42777b23665bbaf3ab2aa9a0a8
hemantksingh <hemantksingh.hk@gmail.com>
2016-08-05 16:55:56 +0100

    Install IIS Server and sub features.

--- configure.ps1
+++ configure.ps1
@@ -10,22 +10,62 @@ function ChocolateyInstalled
   return (Test-Path $chocolateyBin)
 }
 
-if(-not (ChocolateyInstalled)) {
-  Write-Output "Installing Chocolatey..."
-  iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex
-} else {
-  Write-Output "Chocolatey found. Skipping install."
+function ListWindowsFeatures($filter) {
+  Write-Output "Listing '$filter' windows features..."
+
+  $windowsfeatures = choco list -source windowsfeatures
+  $windowsfeatures | ForEach-Object {
+      $line = ""
+      if ($_ -match "\| " + $filter) {
+          $line += $_ -replace " {0,1}\|.*", ""
+          $line
+      }
+  }
 }
 
-$windowsfeatures = choco list -source windowsfeatures
+function GetAllWindowsFeatures ($installed) {
+  Import-Module ServerManager
+  Get-WindowsFeature | Where-Object {$_.Installed -match $installed} | Select-Object -Property Name
+}
 
-$windowsfeatures | ForEach-Object {
+function FeatureInstalled ($feature) {
+  $f = Get-WindowsFeature $feature | Where-Object {$_.Installed -match "True"}
+  return $f.Name -eq $feature
+}
 
-    $line = "choco windowsfeatures "
-
-    if ($_ -match "\| Enabled") {
-
-        $line += $_ -replace " {0,1}\|.*", ""
-        $line
+function AddFeature($feature, $includeAllSubfeatures) {
+    Add-WindowsFeature $feature
+    if($includeAllSubfeatures) {
+        $f = Get-WindowsFeature $feature
+        $subFeatures = $f.SubFeatures -split " "
+        foreach ($subFeature in $subFeatures)
+        {
+          Write-Output "Installing '$subFeature' for '$feature'..."
+          Add-WindowsFeature $subFeature
+        }
     }
 }
+
+if(-not (ChocolateyInstalled)) {
+  Write-Output "Installing Chocolatey ..."
+  iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex
+} else {
+  Write-Output "Skipping 'Chocolatey', it is already installed."
+}
+
+$featuresToInstall = "Web-WebServer", "Web-App-Dev --includeAllSubfeatures", "Web-Mgmt-Tools"
+
+$featuresToInstall | ForEach-Object {
+    $feature = $_.split()[0];
+    $includeAllSubfeatures = $_.split()[1] -eq "--includeAllSubfeatures";
+
+    if( -not (FeatureInstalled $feature)) {
+      Write-Output "Installing '$feature' with 'includeAllSubfeatures=$includeAllSubfeatures' ..."
+      AddFeature $feature $includeAllSubfeatures
+    }
+    else {
+      Write-Output "Skipping '$feature', it is already installed."
+    }
+}
+
+#GetAllWindowsFeatures "False"
